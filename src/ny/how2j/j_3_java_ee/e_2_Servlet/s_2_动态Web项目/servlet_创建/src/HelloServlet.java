619831d2c0f0bc0110a4abd8b324cd053e37e6c7
NewYear <1652492291@qq.com>
2019-10-28 22:42:36 +0800

    springboot--环境搭建，热部署依赖添加。

--- src/ny/how2j/j_3_java_ee/e_2_Servlet/s_2_动态Web项目/servlet_创建/src/HelloServlet.java
+++ src/ny/how2j/j_3_java_ee/e_2_Servlet/s_2_动态Web项目/servlet_创建/src/HelloServlet.java
@@ -10,12 +10,15 @@ public class HelloServlet extends HttpServlet{
     public void doGet(HttpServletRequest request, HttpServletResponse response){
 
         try {
+            response.setContentType("text/html; charset=UTF-8");    //设置这个才能正常显示中文。
             response.getWriter().println("<h1>Hello Servlet!</h1>");
             response.getWriter().println("<h1>文件布置成功</h1>");
 
             response.getWriter().println(new Date().toString());
 
             response.getWriter().println("<h1>服务器启动成功</h1>");
+
+            response.setContentType("text/html; charset=UTF-8");
 
         } catch (IOException e) {
             // TODO Auto-generated catch block
