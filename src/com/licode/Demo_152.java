5839fd14671f918c14946938abe29cbf18211440
NewYear <mail_newyear@163.com>
2020-05-21 17:14:12 +0800

    力扣：152  删掉。。

--- src/com/licode/Demo_152.java
+++ src/com/licode/Demo_152.java
@@ -1,53 +1,5 @@
 package com.licode;
 
 public class Demo_152 {
-    int result = 0 ;
-    boolean flag = false;
-    public int maxProduct(int[] nums) {
-        return son(nums);
-    }
 
-
-    public int son(int[] nums){
-        if (nums.length == 1){
-            return nums[0];
-        }
-        int[] count = new int[nums.length];
-
-        for(int i = 0; i < nums.length -1 ; i++)
-            if ( nums[i+1] == nums[i] +1 ){
-
-                int temp = nums[i] * nums[i+1];
-                son(nums,i+1);
-                if (flag){
-                    if (result < temp){
-                        result = temp;
-                    }
-
-                }
-                if (!flag){
-                    result = temp;
-                    flag = true;
-                }
-
-
-            }
-        return result;
-    }
-
-
-
-    public void son(int[] nums,int index){
-        if( index < 0 || index > nums.length -1 ){
-            return ;
-        }
-        for (int i = index ; i < nums.length -1 ; i++){
-            if ( nums[i+1] == nums[i] +1 ){
-                result = result * nums[i+1];
-                son(nums,i+1);
-            } else {
-                return ;
-            }
-        }
-    }
 }
