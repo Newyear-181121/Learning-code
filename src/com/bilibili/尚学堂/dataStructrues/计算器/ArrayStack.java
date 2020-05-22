dae0fbc007c89b61e631a79060fede621a112147
NewYear <mail_newyear@163.com>
2020-05-22 14:46:51 +0800

    计算表达式，，简单的单项可实现，减法运算的时候会有一点小问题。

--- src/com/bilibili/尚学堂/dataStructrues/计算器/ArrayStack.java
+++ src/com/bilibili/尚学堂/dataStructrues/计算器/ArrayStack.java
@@ -8,27 +8,42 @@ public class ArrayStack {
     private int[] stack;
     private int top = -1;
 
-    //构造器
+    /**
+     * 构造器
+     */
     public ArrayStack(int maxSize){
         this.maxSize = maxSize;
         stack = new int[this.maxSize];
     }
 
-    //栈满
+    /**
+     * 判断栈是否满了。
+     * @return 如果满了，就返回 true。
+     */
     public boolean isfull(){
         return top ==maxSize -1;
     }
 
-    //栈空
+    /**
+     * 判断是否 栈空
+     * @return 如果空，返回 true。
+     */
     public boolean isEmpty(){
         return top == -1;
     }
 
-    // 查看栈顶的数据，但是不出栈。
+    /**
+     * 查看栈顶的数据，但是不出栈。
+      * @return 栈顶中的数
+     */
     public int peek(){
         return stack[top];
     }
-    //
+
+    /**
+     * 入栈
+     * @param value
+     */
     public void push(int value){
         if (isfull()){
             System.out.println("栈满了");
@@ -65,8 +80,12 @@ public class ArrayStack {
         }
     }
 
-    // 返回运算付的优先级，优先级可以自定义，，优先级用数字表示
-    //数字越大，则优先级越高，
+    /**
+     * 返回运算付的优先级，优先级可以自定义，，优先级用数字表示
+     * 数字越大，则优先级越高，
+     * @param oper 运算符
+     * @return  运算符的优先级
+     */
     public int priority(int oper){
         if (oper == '*' || oper == '/'){
             return 1;
@@ -77,12 +96,22 @@ public class ArrayStack {
         }
     }
 
-    //判断 是不是一个运算符
+    /**
+     * 判断 是不是一个运算符
+     * @param val 字符
+     * @return 是  否
+     */
     public boolean isOper(char val) {
         return val == '+' || val == '-' || val == '*' || val == '/';
     }
 
-    // 计算方法
+    /**
+     * 计算
+     * @param num1 数据1
+     * @param num2 数据2
+     * @param oper 运算符
+     * @return 结果
+     */
     public int cal(int num1, int num2, int oper){
         int res = 0; //res ： 结果result ，用于存放计算的结果
         switch (oper){
