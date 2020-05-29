16501d1766c36896a5daeccd0644d1d3edcd7066
NewYear <mail_newyear@163.com>
2020-05-29 20:30:30 +0800

    插入排序

---
+++ src/com/bilibili/尚学堂/sort/插入排序/Test.java
@@ -0,0 +1,32 @@
+package sort.插入排序;
+
+import util.Out;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) {
+        int len = 80000;
+        int[] arr = new int[len];
+        for (int i = 0 ; i < len ; i++){
+            arr[i] = (int) (Math.random() * 10);
+        }
+
+        //System.out.println(Arrays.toString(arr));
+
+
+        System.out.println("排序前");
+        Out.out_time();
+
+        InserSort.inserSort(arr);
+
+        System.out.println("排序后");
+        //System.out.println(Arrays.toString(arr));
+        Out.out_time();
+
+
+        System.out.println("总共比较了："+ InserSort.count);
+    }
+}
--- src/com/bilibili/尚学堂/util/Out.java
+++ src/com/bilibili/尚学堂/util/Out.java
@@ -1,10 +1,24 @@
 package util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * 自定义输出
+ * 封装 输出。
  */
 public class Out {
     public static void out(String str){
         System.out.println(str);
+    }
+
+    /**
+     * 输出执行此方法的时间。
+     */
+    public static void time(){
+        Date date1 = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1str = simpleDateFormat.format(date1);
+        System.out.println(date1str);
     }
 }
