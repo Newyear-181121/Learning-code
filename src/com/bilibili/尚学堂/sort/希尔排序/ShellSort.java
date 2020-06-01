97786d190805608d52a4c27cc0159cf3d7833c88
NewYear <mail_newyear@163.com>
2020-06-01 16:15:48 +0800

    希尔排序。

---
+++ src/com/bilibili/尚学堂/sort/希尔排序/ShellSort.java
@@ -0,0 +1,55 @@
+package sort.希尔排序;
+
+import util.Out;
+
+import java.util.Arrays;
+
+/**
+ *
+ */
+public class ShellSort {
+
+    static int temp = 0;
+    static int count = 0;
+    /**
+     * 希尔排序 交换法
+     * @param arr
+     */
+    public static void shellSort(int[] arr){
+
+        
+        for (int gap = arr.length / 2 ; gap > 0 ; gap /=2 ){
+            for (int i = gap; i < arr.length ; i++){
+                for (int j = i -gap ; j >= 0 ; j-=gap){
+                    if (arr[j] > arr[j + gap]){
+                        temp = arr[j];
+                        arr[j] = arr[j + gap];
+                        arr[j + gap] = temp;
+                    }
+                }
+            }
+            Out.out("希尔排序第" + (++count) + "轮"+ Arrays.toString(arr));
+        }
+    }
+
+    /**
+     * 移位 法
+     * @param arr
+     */
+    public static void shellSort2(int[] arr){
+        for (int gap = arr.length / 2 ; gap > 0 ; gap /=2 ){
+            for (int i = gap; i < arr.length ; i++){
+                int j = i;
+                int temp = arr[j];
+                if (arr[j] < arr[j -gap]){
+                    while (j -gap >= 0 && temp < arr[j - gap]){
+                        arr[j] = arr[j-gap];
+                        j -= gap;
+                    }
+                    arr[j] = temp;
+                }
+            }
+        }
+
+    }
+}
