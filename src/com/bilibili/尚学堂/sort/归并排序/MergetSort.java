f2448908f44b986cf1d7f12eb20097541ad8317b
NewYear <mail_newyear@163.com>
2020-06-18 13:10:08 +0800

    归并排序:successful

---
+++ src/com/bilibili/尚学堂/sort/归并排序/MergetSort.java
@@ -0,0 +1,65 @@
+package sort.归并排序;
+
+import util.Out;
+
+import java.util.Arrays;
+
+public class MergetSort {
+
+    public static void mergeSort(int[] arr,int left,int right,int[] temp){
+        if (left < right ){
+            int mid = (left + right )/2;
+
+            mergeSort(arr,left,mid,temp);
+            mergeSort(arr,mid+1,right,temp);
+            merge(arr,left,mid,right,temp);
+        }
+    }
+
+
+    /**
+     * 合并
+     * @param arr
+     * @param left
+     * @param mid
+     * @param right
+     * @param temp
+     */
+    public static void merge(int[] arr,int left, int mid ,int right,int[] temp){
+        Out.out(Arrays.toString(arr));
+        int i = left;
+        int j = mid +1;
+        int t = 0;
+
+        while ( i <= mid && j <= right ){
+            if (arr[i] <= arr[j] ){
+                temp[t] = arr[i];
+                t += 1;  //临时数组索引 右移
+                i += 1;
+            } else {
+                temp[t] = arr[j];
+                t++;j++;
+            }
+
+        }
+
+
+        while(i <= mid ){
+            temp[t] = arr[i];
+            t++; i++;
+        }
+        while (j <= right ){
+            temp[t] = arr[j];
+            t++; j++;
+        }
+
+
+        t = 0;
+        int tempLeft = left;
+        while(tempLeft <= right ) {
+            arr[tempLeft] = temp[t];
+            t++;
+            tempLeft ++;
+        }
+    }
+}
---
+++ src/com/bilibili/尚学堂/sort/归并排序/Test.java
@@ -0,0 +1,16 @@
+package sort.归并排序;
+
+import util.Out;
+
+import java.util.Arrays;
+
+public class Test {
+
+    @org.junit.Test
+    public void test(){
+        int arr[] = {8,9,5,3,7,6,4,2,4,1};
+        int temp[] = new int[arr.length];
+        MergetSort.mergeSort(arr,0,arr.length-1,temp);
+        Out.out(Arrays.toString(arr));
+    }
+}
