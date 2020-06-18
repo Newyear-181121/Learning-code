4d337d3a00ecad3691167c50094f803bcaf0dd31
NewYear <mail_newyear@163.com>
2020-06-18 21:34:16 +0800

    基数排序:successful

---
+++ src/com/bilibili/尚学堂/sort/基数排序/RadixSort.java
@@ -0,0 +1,50 @@
+package sort.基数排序;
+
+/**
+ * 基数排序 ，是一个经典的空间换时间的排序算法
+ */
+public class RadixSort {
+
+    /**
+     * 对个位数 的 基数排序
+     * @param arr
+     */
+    public static void radixSort_1(int[] arr){
+        // 定义一个二维数组表示10个通，每个桶就是一个一位数组
+        int[][] bucket = new int[10][arr.length];
+        // 记录每个桶中的数据个数
+        int[] bucketElementCounts = new int[10];
+
+        // 按每个数的个位，把数据放到桶中去。
+        for (int j = 0; j < arr.length; j++){
+            int digitofElement = arr[j] % 10 ;
+            bucket[digitofElement][bucketElementCounts[digitofElement]] = arr[j];
+            bucketElementCounts[digitofElement]++;
+        }
+        //把桶中的数据放回原来的数组中来。
+        int index = 0;
+        for(int k = 0 ; k<bucketElementCounts.length; k++){
+            if(bucketElementCounts[k] != 0){
+                for (int l = 0 ; l < bucketElementCounts[k]; l++){
+                    arr[index++] = bucket[k][l];
+                }
+            }
+        }
+
+
+    }
+
+    public static void radixSort(int[] arr){
+        int max = arr[0];
+        for (int i = 1 ; i < arr.length; i++){
+            if (arr[i] > max ){
+                max = arr[i];
+            }
+        }
+        int maxLength = (max + "").length();
+
+        for (int i = 0 , n = 1 ; i < maxLength ; i++ ,n*=10){
+
+        }
+    }
+}
---
+++ src/com/bilibili/尚学堂/sort/基数排序/Test.java
@@ -0,0 +1,14 @@
+package sort.基数排序;
+
+import util.Out;
+
+import java.util.Arrays;
+
+public class Test {
+    @org.junit.Test
+    public void test(){
+        int arr[] = {8,9,1,7,2,3,5,4,6,0};
+        RadixSort.radixSort(arr);
+        Out.out(Arrays.toString(arr));
+    }
+}
