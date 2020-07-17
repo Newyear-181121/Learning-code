f4a6a563747a6342fe6247fe803985d7d5bb2f58
NewYear <mail_newyear@163.com>
2020-07-17 08:59:14 +0800

    完成：基数排序

--- src/com/bilibili/尚学堂/sort/N07基数排序/RadixSort.java
+++ src/com/bilibili/尚学堂/sort/N07基数排序/RadixSort.java
@@ -12,12 +12,14 @@ public class RadixSort {
     public static void radixSort_1(int[] arr){
         // 定义一个二维数组表示10个通，每个桶就是一个一维数组
         int[][] bucket = new int[10][arr.length];
-        //  记录每个桶中的数据个数
+        //  记录每个桶中的数据个数 的值
         int[] bucketElementCounts = new int[10];
 
         // 按每个数的个位，把数据放到桶中去。
         for (int j = 0; j < arr.length; j++){
+            // 个位数的值
             int digitofElement = arr[j] % 10;
+            // 个位数是 。 的桶，个位数是。的桶中有 。。 个数。
             bucket[digitofElement][bucketElementCounts[digitofElement]] = arr[j];
             bucketElementCounts[digitofElement]++;
         }
@@ -43,20 +45,27 @@ public class RadixSort {
         int max = arr[0];
         for (int i = 1 ; i < arr.length; i++){
             if (arr[i] > max ){
-                max = arr[i];
+                max = arr[i]; //数组中最大的数
             }
         }
+        //最大位数
         int maxLength = (max + "").length();
 
+
+
+
         for (int i = 0 , n = 1 ; i < maxLength ; i++ ,n*=10){
+
+            // 这两个定义还起着初始化的作用。
             // 定义一个二维数组表示10个通，每个桶就是一个一位数组
             int[][] bucket = new int[10][arr.length];
             // 记录每个桶中的数据个数
             int[] bucketElementCounts = new int[10];
 
+
             // 按每个数的个位，把数据放到桶中去。
             for (int j = 0; j < arr.length; j++){
-                int digitofElement = arr[j] % 10 ;
+                int digitofElement = arr[j] / n % 10 ;
                 bucket[digitofElement][bucketElementCounts[digitofElement]] = arr[j];
                 bucketElementCounts[digitofElement]++;
             }
@@ -69,6 +78,8 @@ public class RadixSort {
                     }
                 }
             }
+
+            // 放回去之后 初始化
         }
     }
 }
--- src/com/bilibili/尚学堂/sort/N07基数排序/Test.java
+++ src/com/bilibili/尚学堂/sort/N07基数排序/Test.java
@@ -4,10 +4,16 @@ import util.Out;
 
 import java.util.Arrays;
 
+/**
+ *  内存占用大小计算
+ *   一个 int 占四个字节
+ *    8 * 4 / 1024 / 1024 / 1024
+ *              k     M      G
+ */
 public class Test {
     @org.junit.Test
     public void test(){
-        int arr[] = {8,9,1,7,2,3,5,4,6,0};
+        int arr[] = {88,98,81,78,82,38,58,48,86,80};
         RadixSort.radixSort(arr);
         Out.out(Arrays.toString(arr));
     }
