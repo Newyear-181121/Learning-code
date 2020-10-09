169aa45cf243601211f91257ec5f45b4766a82c4
NewYear <yexinnian@banksteel.com>
2020-10-09 14:04:19 +0800

    feat:容器笔记。

---
+++ src/ny/base/collection/Test_01_Collection.java
@@ -0,0 +1,18 @@
+package ny.base.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * @auther: NewYear
+ * @Date: 2020-10-09 13:25
+ * @version: 0.0.1
+ * @description: Test_01_Collention
+ */
+public class Test_01_Collection {
+
+    public static void main(String[] args) {
+        Collection co = new ArrayList();
+    }
+
+}
