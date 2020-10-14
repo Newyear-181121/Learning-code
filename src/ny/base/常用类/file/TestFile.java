ae4da3ddecd9758c1d87f0b427d6022665c7af87
NewYear <yexinnian@banksteel.com>
2020-10-14 21:26:43 +0800

    feat(File):常用的几个方法的测试，获取当前所在路径。

---
+++ src/ny/base/常用类/file/TestFile.java
@@ -0,0 +1,85 @@
+package ny.base.常用类.file;
+
+import ny.base.常用类.myUtil.Out;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * @auther: NewYear
+ * @Date: 2020-10-14 19:37
+ * @version: 0.0.1
+ * @description: TestFile
+ */
+public class TestFile {
+
+    String current; //当前项目绝对路径。
+
+    File f ;
+
+    public static void main(String[] args)  {
+        TestFile tf = new TestFile();
+
+
+        tf.outDir();
+
+        tf.test1();
+
+        tf.test2();
+    }
+
+
+    /**
+     * 输出当前项目绝对路径。
+     */
+    private void outDir() {
+        System.out.println(System.getProperty("user.dir"));
+    }
+
+    /**
+     * file 类常用方法测试。
+     */
+    public void test1()  {
+        current = System.getProperty("user.dir");
+        f = new File(current);
+        Out.out("文件是否存在：",f.exists());
+        Out.out("File是否是目录：",f.isDirectory());
+        Out.out("File是否是文件：",f.isFile());
+        Out.out("File最后修改时间：",new Date(f.lastModified()));
+        Out.out("File的大小：",f.length());
+        Out.out("File的文件名：",f.getName());
+        Out.out("File的目录路径:",f.getPath());
+        Out.out("File的绝对路径是：",f.getAbsolutePath());
+
+        try {
+            Out.out("File的标准路径是：",f.getCanonicalPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 打印 文件夹 中所有的文件和子文件
+     * @param file
+     */
+    public void printFiles(File file,int level){
+        for (int i = 0 ; i<level;i++){
+            System.out.print("*");
+        }
+        System.out.println(file.getName());
+        if (file.isDirectory()){
+            File[] files = file.listFiles();
+            for (File temp:files){
+                printFiles(temp,level+1);   //递归调用。
+            }
+        }
+    }
+
+    private void test2() {
+        printFiles(f,0);
+    }
+
+
+}
--- src/ny/base/常用类/myUtil/Out.java
+++ src/ny/base/常用类/myUtil/Out.java
@@ -4,7 +4,7 @@ package ny.base.常用类.myUtil;
  * @auther: NewYear
  * @Date: 2020-10-13 14:31
  * @version: 0.0.1
- * @description: Out  自定义工具类，
+ * @description: Out  自定义工具类，就是把输出到控制台缩短一点。
  */
 public class Out {
 
@@ -17,5 +17,10 @@ public class Out {
     public static void out(Object object,Object object2){
         System.out.print(object);
         System.out.println(object2);
+    }
+
+    public static void out(String str,Object object){
+        System.out.print(str);
+        System.out.println(object);
     }
 }
