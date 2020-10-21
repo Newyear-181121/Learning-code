20c60a497db21459e942919f577ca68acdfc86e7
NewYear <mail_newyear@163.com>
2020-10-21 20:07:16 +0800

    feat(myHashMap) : 实现简单的  toString 操作.。     put  方法中的标志位补上。

--- src/ny/base/collection/MyHashMap.java
+++ src/ny/base/collection/MyHashMap.java
@@ -7,16 +7,26 @@ import ny.base.常用类.myUtil.Out;
  * @Date: 2020-10-21 16:34
  * @version: 0.0.1
  * @description: MyHashMap 实现 hashMap
+ *
+ * 类里面的方法，就是对自己的属性进行操作的一些方法。
+ *
  */
 public class MyHashMap {
 
     MyHashMapNode[] tables;
     int size;
 
+    /* 构造 */
     public MyHashMap(){
         tables = new MyHashMapNode[16];     //长度一般定义成2的幂数。
     }
 
+    /**
+     * 自定义的 哈希 计算方法。 比较简陋。
+     * @param v
+     * @param length
+     * @return  自定义哈希值
+     */
     public int myHash(int v ,int length){
         Out.out("直接位运算，效率高：",v&(length-1));
         Out.out("取模运算，效率低:",v%(length-1));
@@ -43,6 +53,9 @@ public class MyHashMap {
                 if (temp.key.equals(key)){
                     // 如果 key 相等了，替换 值，并结束循环
                     temp.value = value;
+
+                    keyRepeat = true;               // 提前找到了，要把标志位改掉。
+
                     break;
                 } else {
                     // 如果遍历过程中没有找到 一致的key
@@ -53,12 +66,35 @@ public class MyHashMap {
 
             } // 提前找到了，就提前结束，没有找到就保存尾部 节点。
 
-            if (! keyRepeat) {
+            if (!keyRepeat) {
                 lastNode.next = newNode;
             }
         }
 
+        size++;             // 每次加完记得要把 大小加一。
+    }
 
+    /**
+     *
+     * @return
+     *  使用双层循环 遍历 哈希表，
+     *  外层循环遍历数组，
+     *  内存循环遍历链表。
+     */
+    public String toString(){
+        if (size == 0){
+            return "[]";
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for (MyHashMapNode node : tables){      //
+            while(node != null){
+                sb.append(node.toString()+",");
+                node = node.next;
+            }
+        }
+        sb.setCharAt(sb.length()-1,']');
+        return sb.toString();
     }
 
 
@@ -67,6 +103,10 @@ public class MyHashMap {
         mm.put(123,"qwe");
         mm.put(122,"rwer");
 
+        Out.out(mm);
+
         mm.put(122,"test");
+
+        Out.out(mm);
     }
 }
