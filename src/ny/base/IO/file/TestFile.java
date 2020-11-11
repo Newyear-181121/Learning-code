6f62df28896ff4817c5326d426b731583d7061ff
NewYear <mail_newyear@163.com>
2020-11-11 21:16:25 +0800

    feat(file) :  io file 类常用方法测试实验。

--- src/ny/base/IO/file/TestFile.java
+++ src/ny/base/IO/file/TestFile.java
@@ -1,9 +1,11 @@
 package ny.base.IO.file;
 
 import ny.base.常用类.myUtil.Out;
+import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.Date;
 
 /**
@@ -32,13 +34,14 @@ public class TestFile {
      * 输出当前项目绝对路径。
      */
     private void outDir() {
-        System.out.println(current);
+        System.out.println("当前文件夹路径是："+current);
     }
 
     /**
      * file 类常用方法测试。
      */
     public void test1()  {
+        outDir();
         Out.out("文件是否存在：",f.exists());
         Out.out("File是否是目录：",f.isDirectory());
         Out.out("File是否是文件：",f.isFile());
@@ -56,13 +59,7 @@ public class TestFile {
 
     }
 
-    /**
-     * 打印 文件夹 中所有的文件和子文件
-     * @param file
-     */
-    public void printFiles(File file,int level){
-        printFiles(file,level,0);
-    }
+
 
     /**
      *
@@ -75,7 +72,7 @@ public class TestFile {
             return;
         }
         for (int i = 0 ; i<level;i++){
-            System.out.print("*");
+            System.out.print("—");
         }
         System.out.println(file.getName());
         if (file.isDirectory()){
@@ -86,20 +83,67 @@ public class TestFile {
         }
     }
 
+    /**
+     * 打印 文件夹 中所有的文件和子文件
+     * @param file
+     */
+    public void printFiles(File file,int level){
+        printFiles(file,level,0);
+    }
+
+    public void printFiles(File file){
+        printFiles(file,0,0);
+    }
+
+
+    /**
+     * 测试 遍历子文件夹
+     */
     private void test2() {
         printFiles(f,0,3);
     }
 
+    /**
+     * 添加注解后可以 单独启动测试用例。
+     *
+     *  test  list();
+     */
+    @Test
+    public void test03(){
+        String[] list = f.list();
+        for (String l : list){
+            Out.out(l);
+        }
+    }
+
+    /**
+     * 列出 文件系统的 磁盘根目录
+     */
+    @Test
+    public void listRoot(){
+        File[] files = f.listRoots();
+        for ( File f: files ) {
+            Out.out(f);
+        }
+    }
+
+    @Test
+    public void list(){
+        File[] files = f.listFiles();
+        for ( File f: files ) {
+            Out.out(f);
+        }
+    }
 
 
     public static void main(String[] args)  {
         TestFile tf = new TestFile();
 
 
-        tf.outDir();
-
         tf.test1();
 
         tf.test2();
+
+        //tf.test03();
     }
 }
