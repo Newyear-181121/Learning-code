b58eae4b211d4cbbc34066bcea142a2f4deb92e6
NewYear <mail_newyear@163.com>
2021-04-07 20:09:38 +0800

    feat : 原型模式简单样例

---
+++ src/main/java/ny/设计模式/原型模式/RealizeType.java
@@ -0,0 +1,40 @@
+package ny.设计模式.原型模式;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @auther: NewYear
+ * @Date: 2021-04-07 19:48
+ * @Version: 0.0.1
+ * @Description: RealizeType 原型模式基础实现
+ */
+public class RealizeType implements Cloneable {
+
+    RealizeType(){
+        System.out.println("原型模式实现类实例创建");
+    }
+
+    public RealizeType clone() throws CloneNotSupportedException{
+        System.out.println("clone 原型实例成功");
+        return (RealizeType) super.clone();
+    }
+
+}
+
+
+/**
+ * 测试类
+ */
+class TestRealizeType{
+    @Test
+    public void test(){
+         RealizeType realize1 = new RealizeType();
+        RealizeType realize2 = null;
+        try {
+            realize2 = realize1.clone();
+        } catch (CloneNotSupportedException e) {
+            System.out.println("克隆失败！！！");
+        }
+        System.out.println("realize1==realize2" +"   "+ (realize1==realize2));
+    }
+}
